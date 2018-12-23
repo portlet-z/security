5e5114273ae1bf299478a5aae3e7f3e1fb3dc51f
张新征 <programer.email@gmail.com>
2018-12-23 20:53:15 +0800

    重构用户名密码登录

--- security-app/src/main/java/com/portlet/security/app/authentication/MyAuthenticationSuccessHandler.java
+++ security-app/src/main/java/com/portlet/security/app/authentication/MyAuthenticationSuccessHandler.java
@@ -4,8 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.portlet.security.core.properties.LoginType;
 import com.portlet.security.core.properties.SecurityProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -24,15 +32,60 @@ public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
     @Autowired
     private SecurityProperties securityProperties;
 
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("登录成功");
-        if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));
-        } else {
-            super.onAuthenticationSuccess(request,response,authentication);
+
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
         }
 
+        String[] tokens = extractAndDecodeHeader(header, request);
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId 对应的配置不存在：" + clientId);
+        } else if (StringUtils.equals(clientDetails.getClientSecret(), clientSecret)) {
+            throw new UnapprovedClientAuthenticationException("clientSecret 不匹配" + clientSecret);
+        }
+
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "custom");
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(token));
+
+    }
+
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
+            throws IOException {
+
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException e) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+        String token = new String(decoded, "UTF-8");
+        int delim = token.indexOf(":");
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        }
+        return new String[] { token.substring(0, delim), token.substring(delim + 1) };
     }
 }
--- security-browser/src/main/java/com/portlet/security/browser/BrowserSecurityConfig.java
+++ security-browser/src/main/java/com/portlet/security/browser/BrowserSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.portlet.security.browser;
 
-import com.portlet.security.browser.logout.PortletLogoutSuccessHandler;
-import com.portlet.security.browser.session.PortletExpiredSessionStrategy;
 import com.portlet.security.core.authentication.AbstractChannelSecurityConfig;
 import com.portlet.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
 import com.portlet.security.core.properties.SecurityConstants;
@@ -12,8 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
